f43888f48f4584ac6f248f309cdc8e311b7112cf
Son of Binary <akwotom@hcts.ml>
2024-02-21 05:39:57 +0100

    .
    Fixing a call connection bug, by using on-server call relay (node-turn).

--- calling/public/widgets/call/correspondent.mjs
+++ calling/public/widgets/call/correspondent.mjs
@@ -129,10 +129,6 @@ export default class CallCorrespondent extends Widget {
                     {
                         urls: [
                             'stun:stun.l.google.com:19302',
-                            'stun:stun1.l.google.com:19302',
-                            'stun:stun2.l.google.com:19302',
-                            'stun:stun3.l.google.com:19302',
-                            'stun:stun4.l.google.com:19302'
                         ],
                     },
                     {
